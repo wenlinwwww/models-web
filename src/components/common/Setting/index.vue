44b70078e5af813fb4e88e28b97604b3543a6cf8
wenlinwwww <lynnw7839@gmail.com>
2024-07-09 14:51:48 +0800

    Update index.vue

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -67,14 +67,6 @@ const show = computed({
           </template>
           <aiSetServer />
         </NTabPane>
-        <NTabPane name="Config" tab="Config">
-          <template #tab>
-            <SvgIcon class="text-lg" icon="ri:list-settings-line" />
-            <!-- <span class="ml-2">{{ $t('setting.config') }}</span> -->
-            <span class="ml-2">{{ $t('mjset.about') }}</span>
-          </template>
-          <!-- <About /> -->
-        </NTabPane>
       </NTabs>
     </div>
   </NModal>
