513d5d4c9710c8491b2b2600d7cb1d57c3d0a45e
wenlinwwww <lynnw7839@gmail.com>
2024-07-09 16:30:04 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.png',
+      avatar: 'https://raw.githubusercontent.com/wenlinwwww/chatgpt-web-midjourney-proxy/main/src/assets/avatar.png',
       name: t('mjset.sysname'), // '名字',
       // description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
     },
