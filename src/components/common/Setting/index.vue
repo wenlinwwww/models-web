27c2fd05d6a9671fdabf4964ab0678e14f4b3ae3
wenlinwwww <lynnw7839@gmail.com>
2024-07-22 16:02:27 +0800

    modify ui

--- src/components/common/Setting/index.vue
+++ src/components/common/Setting/index.vue
@@ -2,10 +2,8 @@
 import { computed, ref } from 'vue'
 import { NModal, NTabPane, NTabs } from 'naive-ui'
 import General from './General.vue'
-import Advanced from './Advanced.vue'
 import aiModel from '@/views/mj/aiModel.vue'
 import aiSetServer from '@/views/mj/aiSetServer.vue'
-import About from './About.vue'
 import { homeStore, useAuthStore } from '@/store'
 import { SvgIcon } from '@/components/common'
 
@@ -58,26 +56,25 @@ const show = computed({
           </template>
           <div class="min-h-[100px]">
             <!-- <Advanced /> -->
-            <aiModel/>
+            <aiModel />
           </div>
         </NTabPane>
 
-        <NTabPane name="server" tab="server" v-if=" ! homeStore.myData.session.isHideServer">
+        <NTabPane v-if=" !homeStore.myData.session.isHideServer" name="server" tab="server">
           <template #tab>
             <SvgIcon class="text-lg" icon="mingcute:server-line" />
             <span class="ml-2">{{ $t('mjset.server') }}</span>
           </template>
           <aiSetServer />
         </NTabPane>
-        <NTabPane name="Config" tab="Config">
+        <!-- <NTabPane name="Config" tab="Config">
           <template #tab>
             <SvgIcon class="text-lg" icon="ri:list-settings-line" />
-            <!-- <span class="ml-2">{{ $t('setting.config') }}</span> -->
+            <span class="ml-2">{{ $t('setting.config') }}</span>
             <span class="ml-2">{{ $t('mjset.about') }}</span>
           </template>
           <About />
-        </NTabPane>
-
+        </NTabPane> -->
       </NTabs>
     </div>
   </NModal>
--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Dooy/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
-      name:  t('mjset.sysname'),//'AI绘图',
-      description: 'Star on <a href="https://github.com/Dooy/chatgpt-web-midjourney-proxy" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://raw.githubusercontent.com/wenlinwwww/chatgpt-web-midjourney-proxy/main/src/assets/avatar.jpg',
+      name: t('mjset.sysname'), // 'AI绘图',
+      description: '<a href="https://docs.qq.com/doc/DVklWTURKcXJOdWlq" class="text-blue-500" target="_blank" >使用手册</a>',
     },
   }
 }
